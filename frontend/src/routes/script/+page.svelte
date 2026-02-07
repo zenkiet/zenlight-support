faf64f8a16d5bc3679966b61f76cb4858dd0ed07
Kiet Le <kietle@blogicsystems.com>
2026-02-07 15:35:19 +0700

    feat: ✨ implement virual scroll result table and limit 1000 records return

--- frontend/src/routes/script/+page.svelte
+++ frontend/src/routes/script/+page.svelte
@@ -2,7 +2,7 @@
 	import ResultTable from '$lib/components/templates/script/ResultTable.svelte';
 	import SqlEditor from '$lib/components/templates/script/SqlEditor.svelte';
 	import { scriptStore } from '$lib/stores/script.svelte';
-	import { onMount } from 'svelte';
+	import { onDestroy, onMount } from 'svelte';
 	import { format } from 'sql-formatter';
 
 	let data = $state('');
@@ -14,19 +14,24 @@
 		scriptStore.init();
 	});
 
+	onDestroy(() => {
+		console.log('Resetting script store on unmount');
+		scriptStore.reset();
+	});
+
 	function formatScript() {
-    if (!data || !data.trim()) return;
-    try {
-      data = format(data, {
-        language: 'transactsql',
-        keywordCase: 'upper',
-        tabWidth: 2,
-        linesBetweenQueries: 2
-      });
-    } catch (error) {
-      console.error('Format error:', error);
-    }
-  }
+		if (!data || !data.trim()) return;
+		try {
+			data = format(data, {
+				language: 'transactsql',
+				keywordCase: 'upper',
+				tabWidth: 2,
+				linesBetweenQueries: 2
+			});
+		} catch (error) {
+			console.error('Format error:', error);
+		}
+	}
 </script>
 
 <header class="mb-6 flex shrink-0 flex-col gap-5 pt-2">
@@ -70,7 +75,7 @@
 
 		<div class="flex h-10 items-center space-x-3 bg-surface px-4 transition-colors duration-300">
 			<button
-				class="cursor-pointer rounded text-muted hover:text-primary disabled:text-muted/30 disabled:cursor-not-allowed"
+				class="cursor-pointer rounded text-muted hover:text-primary disabled:cursor-not-allowed disabled:text-muted/30"
 				aria-label="Run Script"
 				onclick={() => scriptStore.execute(data)}
 				disabled={scriptStore.loading || !data.trim()}
@@ -85,7 +90,7 @@
 			<div class="mx-2 h-6 w-px bg-border"></div>
 
 			<button
-				class="cursor-pointer text-muted hover:text-main disabled:text-muted/30 disabled:cursor-not-allowed"
+				class="cursor-pointer text-muted hover:text-main disabled:cursor-not-allowed disabled:text-muted/30"
 				aria-label="Format Script"
 				onclick={formatScript}
 				disabled={scriptStore.loading || !data.trim()}
